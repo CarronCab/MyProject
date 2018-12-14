8517aa30ae06d263a2b5a7738671259bc33a7365
CarronCab <charles.carroncabaret@gmail.com>
2018-12-14 14:15:50 +0100

    save

---
+++ app/src/main/java/com/example/charl/myproject/HeroDetails.java
@@ -0,0 +1,14 @@
+package com.example.charl.myproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HeroDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+    }
+}
